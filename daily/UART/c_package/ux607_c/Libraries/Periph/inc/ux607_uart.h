9acd1193c041e954b41946e4e46e392f099f5830
hust-gaoyujing <1474257100@qq.com>
2021-04-09 00:02:24 +0800

    210409

--- daily/UART/c_package/ux607_c/Libraries/Periph/inc/ux607_uart.h
+++ daily/UART/c_package/ux607_c/Libraries/Periph/inc/ux607_uart.h
@@ -8,7 +8,7 @@
   
 typedef struct
 {
-	uint32_t UART_Divisor;           		 /*!< This member configures the UART communication baud rate.
+	uint32_t UART_BaudRate;           		 /*!< This member configures the UART communication baud rate.
 												The baud rate is computed using the following formula:
 													- IntegerDivider = ((PCLKx) / (16 * (UART_InitStruct->UART_BaudRate)))
 													- FractionalDivider = ((IntegerDivider - ((u32) IntegerDivider)) * 16) + 0.5 */
@@ -38,64 +38,64 @@ typedef struct
   * @}
   */ 
 
-/** @defgroup USART_No_Parity 
+/** @defgroup UART_No_Parity 
   * @{
   */ 
   
-#define USART_No_Parity                      ((uint32_t)0x1000)
-#define USART_Has_Parity                     ((uint32_t)0x0000)
+#define UART_No_Parity                      ((uint32_t)0x1000)
+#define UART_Has_Parity                     ((uint32_t)0x0000)
 /**
   * @}
   */ 
 
-/** @defgroup USART_Parity_Type 
+/** @defgroup UART_Parity_Type 
   * @{
   */ 
   
-#define USART_Parity_Odd                     ((uint32_t)0x00000) 
-#define USART_Parity_Even                    ((uint32_t)0x10000)
+#define UART_Parity_Odd                     ((uint32_t)0x00000) 
+#define UART_Parity_Even                    ((uint32_t)0x10000)
 /**
   * @}
   */ 
 
-/** @defgroup USART_Enable 
+/** @defgroup UART_Enable 
   * @{
   */ 
   
-#define USART_Enable   	                     ((uint32_t)0x0200)
-#define USART_Disable   	                 ((uint32_t)0x0000)
+#define UART_Enable   	                    ((uint32_t)0x0200)
+#define UART_Disable   	                 	((uint32_t)0x0000)
 /**
   * @}
   */ 
 
-/** @defgroup USART_Mode 
+/** @defgroup UART_Mode 
   * @{
   */ 
   
-#define USART_Mode_Rx                        ((uint32_t)0x0100)
-#define USART_Mode_Tx                        ((uint32_t)0x0010)
+#define UART_Mode_Rx                        ((uint32_t)0x0100)
+#define UART_Mode_Tx                        ((uint32_t)0x0010)
 /**
   * @}
   */ 
 
-/** @defgroup USART_Flags 
+/** @defgroup UART_Flags 
   * @{
   */
 
-#define USART_FLAG_RX_OK                     ((uint32_t)0x0010)
-#define USART_FLAG_TX_OK                     ((uint32_t)0x0001)
+#define UART_FLAG_RX_OK                     ((uint32_t)0x0010)
+#define UART_FLAG_TX_OK                     ((uint32_t)0x0001)
 /**
   * @}
   */ 
 
 
-/** @defgroup USART_Exported_Functions
+/** @defgroup UART_Exported_Functions
   * @{
   */
 
-void USART_Init(USART_TypeDef* USARTx, USART_InitTypeDef* USART_InitStruct);
-void USART_StructInit(USART_InitTypeDef* USART_InitStruct);
-void USART_Cmd(USART_TypeDef* USARTx, FunctionalState NewState);
-FlagStatus USART_GetFlagStatus(USART_TypeDef* USARTx, uint16_t USART_FLAG);
-void USART_SendData(USART_TypeDef* USARTx, uint16_t Data);
-void USART_ReceiverWakeUpCmd(USART_TypeDef* USARTx, FunctionalState NewState);
+void UART_Init(UART_TypeDef* UARTx, UART_InitTypeDef* UART_InitStruct);
+void UART_StructInit(UART_InitTypeDef* UART_InitStruct);
+void UART_Cmd(UART_TypeDef* UARTx, FunctionalState NewState);
+FlagStatus UART_GetFlagStatus(UART_TypeDef* UARTx, uint16_t UART_FLAG);
+void UART_SendData(UART_TypeDef* UARTx, uint16_t Data);
+void UART_ReceiverWakeUpCmd(UART_TypeDef* UARTx, FunctionalState NewState);
