0c8bfccd6524e60ea30af77b54945a5c16a4cbb1
hust-gaoyujing <1474257100@qq.com>
2021-10-14 00:04:57 +0800

    modify tb

--- daily/uart-chisel/rtl/gyj_uart_tb.v
+++ daily/uart-chisel/rtl/gyj_uart_tb.v
@@ -3,22 +3,30 @@
 
 
 module uart_tb();
-	reg   							clk;
-	reg   							rst_n;
-	
-	reg                      		cmd_valid;
-	wire                     		cmd_ready;
-	reg  [32-1:0]	    			cmd_addr; 
-	reg                      		cmd_read; 
-	reg  [31:0]            			cmd_wdata;
-		
-	wire                     		rsp_valid;
-	reg                      		rsp_ready;
-	wire [31:0]            			rsp_rdata;
-	
-	wire  							interrupts_0_0;                
-	wire  							port_txd;
-	reg		 						port_rxd;
+
+  	reg         clK;
+  	reg         rst;
+  	wire        arready;
+  	reg         arvalid;
+  	reg  [31:0] araddr;
+  	reg  [2:0]  arprot;
+  	reg         rready;
+  	wire        rvalid;
+  	wire [31:0] rdata;
+  	wire [1:0]  rresp;
+  	wire        awready;
+  	reg         awvalid;
+  	reg  [31:0] awaddr;
+  	reg  [2:0]  awprot;
+  	wire        wready;
+  	reg         wvalid;
+  	reg  [31:0] wdata;
+  	reg  [3:0]  wstrb;
+  	reg         bready;
+  	wire        bvalid;
+  	wire [1:0]  bbits_resp;
+  	reg         port_rxd;
+  	wire        port_txd;
 	
 	reg [7:0]	data_in[0:255];
 	reg	[7:0]	data_out[0:255];
@@ -36,102 +44,66 @@ module uart_tb();
 	
 	//VARIABLE
 	reg [8*300:1] 	FREQUENCE;
-	reg [8*300:1] 	BAUDRATE;
 	reg [8*300:1] 	TEST_TYPE;
-	reg [3:0] 		BAUD_EN;
-	reg [3:0] 		TX_EN;
-	reg [0:0] 		RX_EN;
-	reg [2:0] 		UART_EN;
-	reg [3:0] 		NO_PARITY;
-	reg [3:0] 		EV_PARITY;
-	
-	gjy_uart_top	uart_1(
-	.clk					(clk				),
-	.rst_n                  (rst_n              ),
-												
-	.i_icb_cmd_valid        (cmd_valid          ),
-	.i_icb_cmd_ready        (cmd_ready          ),
-	.i_icb_cmd_addr         (cmd_addr           ),
-	.i_icb_cmd_read         (cmd_read           ),
-	.i_icb_cmd_wdata        (cmd_wdata          ),
-												
-	.i_icb_rsp_valid        (rsp_valid          ),
-	.i_icb_rsp_ready        (rsp_ready          ),
-	.i_icb_rsp_rdata        (rsp_rdata          ),
-												
-	.io_interrupts_0_0      (interrupts_0_0     ),     
-	.io_port_txd            (port_txd           ),
-	.io_port_rxd            (port_rxd           )
+	reg [2:0] 		CLK_EN;
+	reg [3:0] 		PARITY_EN;
+	reg [3:0] 		PARITY_EVEN;
+	reg [8*300:1] 	BAUDRATE;
+
+	Axi4LiteUartTop uart_1(
+	  	.clock							(clk				),
+	  	.reset							(rst				),
+	  	.io_axi_readAddr_ready			(arready			),
+	  	.io_axi_readAddr_valid			(arvalid			),
+	  	.io_axi_readAddr_bits_addr		(ardata				),
+	  	.io_axi_readAddr_bits_prot		(arprot 			),
+	  	.io_axi_readData_ready			(rready 			),
+	  	.io_axi_readData_valid			(rvalid 			),
+	  	.io_axi_readData_bits_data		(rdata				),
+	  	.axi_readData_bits_resp			(rresp  			),
+	  	.io_axi_writeAddr_ready			(awready 			),
+	  	.io_axi_writeAddr_valid			(awvalid			),
+	  	.io_axi_writeAddr_bits_addr		(awaddr 			),
+	  	.io_axi_writeAddr_bits_prot		(awprot				),
+	  	.io_axi_writeData_ready			(wready 			),		
+	  	.io_axi_writeData_valid			(wvalid 			),		
+	  	.io_axi_writeData_bits_data		(wdata 	     		),
+	  	.io_axi_writeData_bits_strb		(wstrb  			),
+	  	.io_axi_writeResp_ready			(bready 			),		
+	  	.io_axi_writeResp_valid			(bvalid 			),		
+	  	.io_axi_writeResp_bits_resp		(bresp	     		),
+	  	.io_rxd							(port_rxd  			),
+	  	.io_txd							(port_txd           )
 	);
-	
-Axi4LiteUartTop uart_1(
-  input         clock,
-  input         reset,
-  output        io_axi_readAddr_ready,
-  input         io_axi_readAddr_valid,
-  input  [31:0] io_axi_readAddr_bits_addr,
-  input  [2:0]  io_axi_readAddr_bits_prot,
-  input         io_axi_readData_ready,
-  output        io_axi_readData_valid,
-  output [31:0] io_axi_readData_bits_data,
-  output [1:0]  io_axi_readData_bits_resp,
-  output        io_axi_writeAddr_ready,
-  input         io_axi_writeAddr_valid,
-  input  [31:0] io_axi_writeAddr_bits_addr,
-  input  [2:0]  io_axi_writeAddr_bits_prot,
-  output        io_axi_writeData_ready,
-  input         io_axi_writeData_valid,
-  input  [31:0] io_axi_writeData_bits_data,
-  input  [3:0]  io_axi_writeData_bits_strb,
-  input         io_axi_writeResp_ready,
-  output        io_axi_writeResp_valid,
-  output [1:0]  io_axi_writeResp_bits_resp,
-  input         io_rxd,
-  output        io_txd
-);
 
 
 	initial begin
 		clk  <=1'b0;
-		rst_n <=1'b0;
-		#2000 rst_n <=1'b1;
-		#2000 rst_n <=1'b0;
-		#2000 rst_n <=1'b1;
+		rst <=1'b1;
+		#2000 rst <=1'b0;
+		#2000 rst <=1'b1;
+		#2000 rst <=1'b0;
 	end
 
 	initial begin 
 		if($value$plusargs("FREQUENCE=%s",FREQUENCE))
 			$display("FREQUENCE=%0s",FREQUENCE);
 		
-		
-		if($value$plusargs("BAUDRATE=%0s",BAUDRATE))
-			$display("BAUDRATE=%0s",BAUDRATE);
-		
 		if($value$plusargs("TEST_TYPE=%0s",TEST_TYPE))
 			$display("TEST_TYPE=%0s",TEST_TYPE);
 		
-		if($value$plusargs("BAUD_EN=%h",BAUD_EN))
-			$display("BAUD_EN=%4h",BAUD_EN);
-		
-		
-		if($value$plusargs("TX_EN=%h",TX_EN))
-			$display("TX_EN=%h",TX_EN);
-				
-		
-		if($value$plusargs("RX_EN=%h",RX_EN))
-			$display("RX_EN=%h",RX_EN);
-				
-		if($value$plusargs("UART_EN=%h",UART_EN))
+		if($value$plusargs("CLk_EN=%h",UART_EN))
 			$display("UART_EN=%h",UART_EN);
 				
-		
-		if($value$plusargs("NO_PARITY=%h",NO_PARITY))
-			$display("NO_PARITY=%h",NO_PARITY);
+		if($value$plusargs("PARITY_EN=%h",PARITY_EN))
+			$display("PARITY_EN=%h",PARITY_EN);
 			
+		if($value$plusargs("PARITY_EVEN=%h",PARITY_EVEN))
+			$display("PARITY_EVEN=%h",PARITY_EVEN);
 		
-		if($value$plusargs("EV_PARITY=%h",EV_PARITY))
-			$display("EV_PARITY=%h",EV_PARITY);
-			
+		if($value$plusargs("BAUDRATE=%0s",BAUDRATE))
+			$display("BAUDRATE=%0s",BAUDRATE);
+					
 		
 		if(FREQUENCE == "16M")  
 			forever #31.25 clk <= ~clk;		//for 16Mhz
@@ -151,16 +123,9 @@ Axi4LiteUartTop uart_1(
 		$finish;
 	end 
 	
-	//rsp_ready
-	always @(posedge clk or negedge rst_n)
-		if(rst_n) 
-			rsp_ready <= 1'b0;
-		else begin 
-			if(rsp_valid)
-					rsp_ready <= 1'b1;
-				else 
-					rsp_ready <= 1'b0;
-		end 
+	//rready and bready(assign 1'b1 all the time)
+	rready = 1'b1;
+	bready = 1'b1;
 	
 	//LOAD DATA TO DATA_TX
 	initial begin 
@@ -211,8 +176,8 @@ Axi4LiteUartTop uart_1(
 	
 	
 	task read_register;
-		input  	[`UX607_PA_SIZE-1:0]	    addr;
-		input 								over;
+		input  	[31:0]	    addr;
+		input 				over;
 		
 		begin
 	//cmd	
@@ -241,7 +206,7 @@ Axi4LiteUartTop uart_1(
 	endtask
 
 	task write_register;
-		input  	[`UX607_PA_SIZE-1:0]	    addr; 
+		input  	[31:0]	    addr; 
 		input  	[31:0]            			wdata;
 		input 								over;
 		
@@ -301,7 +266,7 @@ Axi4LiteUartTop uart_1(
 				end
 			end			
 			//UART_CTRL	
-			write_register(`UART_CTRL_ADDR,{EV_PARITY,NO_PARITY,UART_EN,RX_EN,TX_EN,BAUD_EN},1);
+			write_register(`UART_CTRL_ADDR,{PARITY_EVEN,PARITY_EN,UART_EN,RX_EN,TX_EN,BAUD_EN},1);
 			#10000;
 		end 
 	endtask
@@ -342,7 +307,7 @@ Axi4LiteUartTop uart_1(
 						for(j = 0;j < 8;j = j + 1)begin 
 							#8681	port_rxd = data_tmp[j];		//data
 						end 
-						if(NO_PARITY != 1) begin
+						if(PARITY_EN != 1) begin
 							#8681 	port_rxd <= even_parity;			//parity_bit
 						end 
 						#8681 	port_rxd <= 1'b1;				//stop_bit
